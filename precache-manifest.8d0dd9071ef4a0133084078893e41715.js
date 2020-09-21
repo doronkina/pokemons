5451ea5f2ed217c9633321f83c2650b409a08a5b
doronkina <liudmyla.doronkina0@gmail.com>
2020-09-21 18:11:59 +0300

    Updates

---
+++ precache-manifest.8d0dd9071ef4a0133084078893e41715.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a649e077496adf1f4a974d10eb9ef9",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "4c73ccbd69d45706f59e",
+    "url": "/pokemons/static/css/main.c1add366.chunk.css"
+  },
+  {
+    "revision": "efadded44ef13800a81f",
+    "url": "/pokemons/static/js/2.5828d3e6.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/pokemons/static/js/2.5828d3e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c73ccbd69d45706f59e",
+    "url": "/pokemons/static/js/main.78ef8d35.chunk.js"
+  },
+  {
+    "revision": "b2629a5c54af193e8c39",
+    "url": "/pokemons/static/js/runtime-main.22858f6c.js"
+  },
+  {
+    "revision": "651ebad9d2ca484f845099f07ecadcfa",
+    "url": "/pokemons/static/media/logo.651ebad9.png"
+  },
+  {
+    "revision": "5d396d0039f127fcddf9204f085b2b26",
+    "url": "/pokemons/static/media/pokeball.5d396d00.png"
+  }
+]);
